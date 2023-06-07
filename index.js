9d5c6463b34b3d46b47a1a1b1ea01c22d9fd9d90
FowziaSultana <fowziasultana168@gmail.com>
2023-06-08 00:46:16 +0600

    Added get post method for users

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -13,3 +14,49 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`FASHION HOUSE is running on port: ${port}`);
 });
+
+const pass = process.env.DOC_PASS;
+const user = process.env.DOC_USER;
+
+const uri = `mongodb+srv://${user}:${pass}@cluster0.rbjgw7e.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usersCollection = client.db("fashion-house-db").collection("users");
+
+    //get all users
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    //create a single user
+    app.post("/users", async (req, res) => {
+      const singleUser = req.body;
+      console.log(singleUser);
+      const result = await usersCollection.insertOne(singleUser);
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
