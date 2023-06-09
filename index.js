0e2a74baae71492198fb524a4e5c92a9192a52c4
FowziaSultana <fowziasultana168@gmail.com>
2023-06-09 23:57:48 +0600

    added more functions

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send(`FASHION HOUSE is running `);
-});
 
-app.listen(port, () => {
-  console.log(`FASHION HOUSE is running on port: ${port}`);
-});
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const pass = process.env.DOC_PASS;
 const user = process.env.DOC_USER;
@@ -32,9 +47,19 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
-    const usersCollection = client.db("fashion-house-db").collection("users");
+    const usersCollection = client.db("Fashion-House-DB").collection("users");
+    const classCollection = client.db("Fashion-House-DB").collection("class");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.send({ token });
+    });
 
     //get all users
     app.get("/users", async (req, res) => {
@@ -44,11 +69,118 @@ async function run() {
 
     //create a single user
     app.post("/users", async (req, res) => {
-      const singleUser = req.body;
-      console.log(singleUser);
-      const result = await usersCollection.insertOne(singleUser);
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "This user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    //checking whether the user is admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+        // console.log("sended false");
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+      //console.log(result);
+    });
+    //checking whether the user is instructor
+    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false });
+        //console.log("sended false");
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+      // console.log(result);
+    });
+    //checking whether the user is student
+    app.get("/users/student/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ student: false });
+        //console.log("sended false");
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { student: user?.role === "student" };
+      res.send(result);
+      //console.log(result);
+    });
+
+    //checking all type user role
+    app.get("/checkuser", async (req, res) => {
+      if (req.query?.email) {
+        if (req.query.email == "undefined") {
+          console.log("undefined");
+          res.send({});
+        } else {
+          let mail = req.query.email;
+          const query = { email: mail };
+          const result = await usersCollection.findOne(query);
+          res.send(result);
+        }
+      } else {
+        console.log("no mail");
+        res.send({});
+      }
+    });
+
+    //update user to admin
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    //update user to Instructor
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      //console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    //==================================CLASS API==================================================
+
+    app.post("/classes", verifyJWT, async (req, res) => {
+      const newItem = req.body;
+      console.log("from add claass", newItem);
+      const result = await classCollection.insertOne(newItem);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -60,3 +192,10 @@ async function run() {
   }
 }
 run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send(`FASHION HOUSE is running `);
+});
+
+app.listen(port, () => {
+  console.log(`FASHION HOUSE is running on port: ${port}`);
+});
